6f0eb292cb2e235d0ea8ab5da04381f16974fa5e
Alexander Lichter <github@lichter.io>
2022-12-02 13:42:39 +0100

    refactor: basic image transformation
    Co-authored-by: Sébastien Chopin <seb@nuxtlabs.com>

--- pages/[name].vue
+++ pages/[name].vue
@@ -15,12 +15,16 @@ useHead({
     { name: 'description', content: repo.value.repo.description }
   ]
 })
+
+readme.value.html = readme.value.html.replace(/src="\./g, `src="https://raw.githubusercontent.com/${repo.value.repo.repo}/main/.`)
 </script>
 
 <template>
   <div class="project-detail">
-    <NuxtLink :href="`https://github.com/${repo.repo.repo}`" class="project-gh" target="_blank"><span>Check out</span><strong>{{ repo.repo.name }}</strong> on Github
-      <GithubLogo /> &mdash; <Star />stars
+    <NuxtLink :href="`https://github.com/${repo.repo.repo}`" class="project-gh" target="_blank"><span>Check
+        out</span><strong>{{ repo.repo.name }}</strong> on Github
+      <GithubLogo /> &mdash;
+      <Star />stars
       {{ formatStarCount(repo.repo.stars) }}
     </NuxtLink>
     <div class="readme" v-html="readme.html" />
@@ -48,10 +52,12 @@ useHead({
   @media screen and (max-width:767px) {
     width: 100%;
     justify-content: center;
+
     span {
       display: none;
     }
   }
+
   svg {
     width: 16px;
     height: 16px;
@@ -61,7 +67,7 @@ useHead({
 
 <style lang="scss">
 .readme {
-  & > *:first-child {
+  &>*:first-child {
     margin-top: 0;
   }
 
@@ -78,6 +84,7 @@ useHead({
       font-size: 1.65rem;
       line-height: 1.25;
     }
+
     h2 {
       font-size: 1.35rem;
       line-height: 1.25;
@@ -98,6 +105,7 @@ useHead({
     padding-left: 8px;
     margin: 0 0 16px;
   }
+
   p,
   blockquote,
   ul,
@@ -109,14 +117,16 @@ useHead({
     margin-top: 0;
     margin-bottom: 16px;
   }
+
   ul,
   ol {
     margin-inline-start: 24px;
     line-height: 1.2;
+
     a {
       &:hover {
         text-decoration: underline;
-      }   
+      }
     }
   }
 
@@ -127,11 +137,14 @@ useHead({
     max-width: 100%;
     overflow: auto;
     border-collapse: collapse;
+
     tr {
       border-color: hsl(var(--black) / 0.1);
       border-top: 1px solid hsl(var(--black) / 0.1);
     }
-    th, td {
+
+    th,
+    td {
       border: 1px solid hsl(var(--black) / 0.1);
       padding: 8px 16px;
       font-weight: 600;
@@ -144,7 +157,7 @@ useHead({
 
   .highlight {
     overflow: auto;
-    background: rgba(0,0,0,.05);
+    background: rgba(0, 0, 0, .05);
     padding: 1rem;
     margin-bottom: 1rem;
   }
