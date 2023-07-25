4771f1ce59c416005707919f3605fa3abcf189a1
barbapapazes <e.soubiran25@gmail.com>
2023-07-25 18:40:04 +0200

    chore: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/guide/directory-structure/nuxt.config#nuxt-config-file
+export default defineNuxtConfig({
+  modules: ['@nuxt/content'],
+  content: {
+    documentDriven: true
+  }
+})
