1fc6c05e28577e776571783d64ae1b4cf60b1866
Enkot <taras.batenkov@gmail.com>
2023-08-19 17:54:38 +0300

    fix: puzzle transition (#57)

--- components/puzzle/Puzzle.vue
+++ components/puzzle/Puzzle.vue
@@ -6,31 +6,25 @@ const props = defineProps<{
   away: PuzzlePart
 }>()
 
-const firstLine = computed(() => props.parts.slice(0, 3))
-const secondLine = computed(() => props.parts.slice(3, 6))
-const thirdLine = computed(() => props.parts.slice(6, 9))
+const rows = computed(() => [
+  [{} as PuzzlePart, ...props.parts.slice(0, 3)],
+  [props.away, ...props.parts.slice(3, 6)],
+  [{} as PuzzlePart, ...props.parts.slice(6, 9)],
+])
+
+const transforms = [
+  ['', 'translate-x--13 translate-y--8 rotate--20', 'translate-y--8', 'translate-x-7 translate-y--7'],
+  ['translate-y-56 translate-x--50 xl:translate-y-48 xl:translate-x--120 rotate--25', 'translate-x--7', '', 'translate-x-7'],
+  ['', 'translate-x--7 translate-y-8', 'translate-y-8', 'translate-x-7 translate-y-8'],
+]
 </script>
 
 <template>
-  <div relative>
-    <div flex="~ col" space-y--1 drop-shadow-md hover="space-y--9 drop-shadow-xl" class="group peer">
-      <div class="puzzle-row">
-        <PuzzlePart v-for="(part, index) in firstLine" :key="part.name" :part="part" :class="{ 'relative left--6 rotate--20 group-hover:left-0 group-hover:rotate-0': index === 0 }" />
-      </div>
-      <div class="puzzle-row">
-        <PuzzlePart v-for="part in secondLine" :key="part.name" :part="part" />
-      </div>
-      <div class="puzzle-row">
-        <PuzzlePart v-for="part in thirdLine" :key="part.name" :part="part" />
+  <div relative flex justify-end w-84 h-84 class="group">
+    <div flex="~ col" space-y--9 transition-filter ease-in duration-200 drop-shadow-md group-hover:drop-shadow-xl pointer-events-none>
+      <div v-for="(row, i) in rows" :key="i" flex flex-row space-x--9 transition-transform ease-in duration-200>
+        <PuzzlePart v-for="(part, j) in row" :key="part.name" :part="part" :class="[`${transforms[i][j]} group-hover:(translate-0 rotate-0)`, { '<lg:opacity-0': !j }]" shrink-0 />
       </div>
     </div>
-    <!-- 27px and 23px are magics numbers -->
-    <PuzzlePart opacity-0 lg:opacity-100 absolute bottom--20 xl:bottom--12 rotate--25 class="left-[calc(2*-136px)] xl:left-[calc(4*-136px)] peer-hover:bottom-[calc(1*136px-27px)] peer-hover:left-[calc(0.5*-136px-23px)]" peer-hover:rotate-0 :part="away" />
   </div>
 </template>
-
-<style scoped>
-.puzzle-row {
-  --at-apply: flex flex-row space-x--2 group-hover:space-x--9 transition-all ease-in duration-200
-}
-</style>
