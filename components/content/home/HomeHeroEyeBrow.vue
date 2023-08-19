500d0211109588ae0be46b0e53d03f8552f62352
Estéban <e.soubiran25@gmail.com>
2023-08-19 16:53:25 +0200

    fix: update home hero eye brow (#59)

---
+++ components/content/home/HomeHeroEyeBrow.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+  prefix: string
+  title: string
+  path: string
+}>()
+</script>
+
+<template>
+  <NuxtLink :to="path" flex="~" gap-4>
+    <span p="x-3 y-1" bg="gray-900 opacity-10" border="~ gray-900 opacity-40" rounded="full" text="sm gray-900" font="medium">
+      {{ prefix }}
+    </span>
+    <span flex="~ row items-center" gap-2>
+      {{ title }}
+      <span i-heroicons-chevron-right-20-solid w-5 h-5 />
+    </span>
+  </NuxtLink>
+</template>
