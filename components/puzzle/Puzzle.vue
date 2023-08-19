ac499d794917efa4e0608b6f9d15a634a9321a6d
Enkot <taras.batenkov@gmail.com>
2023-08-19 12:46:29 +0300

    fix: use group hover; lg breakpoint

--- components/puzzle/Puzzle.vue
+++ components/puzzle/Puzzle.vue
@@ -6,8 +6,6 @@ const props = defineProps<{
   away: PuzzlePart
 }>()
 
-const triggered = ref(false)
-
 const rows = computed(() => [
   [{} as PuzzlePart, ...props.parts.slice(0, 3)],
   [props.away, ...props.parts.slice(3, 6)],
@@ -16,18 +14,17 @@ const rows = computed(() => [
 
 const transforms = [
   ['', 'translate-x--13 translate-y--8 rotate--20', 'translate-y--8', 'translate-x-7 translate-y--7'],
-  ['translate-y-48 translate-x--120 rotate--25', 'translate-x--7', '', 'translate-x-7'],
+  ['translate-y-56 translate-x--50 xl:translate-y-48 xl:translate-x--120 rotate--25', 'translate-x--7', '', 'translate-x-7'],
   ['', 'translate-x--7 translate-y-8', 'translate-y-8', 'translate-x-7 translate-y-8'],
 ]
 </script>
 
 <template>
-  <div relative flex justify-end w-84>
-    <div flex="~ col" space-y--9 :class="triggered ? 'drop-shadow-xl' : 'drop-shadow-md'">
+  <div relative flex justify-end w-84 h-84 class="group">
+    <div absolute top-0 right-0 flex="~ col" space-y--9 transition-filter ease-in duration-200 drop-shadow-md group-hover:drop-shadow-xl pointer-events-none>
       <div v-for="(row, i) in rows" :key="i" flex flex-row space-x--9 transition-transform ease-in duration-200>
-        <PuzzlePart v-for="(part, j) in row" :key="part.name" :part="part" :class="{ [transforms[i][j]]: !triggered, '<lg:opacity-0': !j }" shrink-0 />
+        <PuzzlePart v-for="(part, j) in row" :key="part.name" :part="part" :class="[`${transforms[i][j]} group-hover:(translate-0 rotate-0)`, { '<lg:opacity-0': !j }]" shrink-0 />
       </div>
     </div>
-    <div absolute inset-0 @mouseenter="triggered = true" @mouseleave="triggered = false" />
   </div>
 </template>
