c34e2289f5eab7a3a5bf91bc6a8e5d085d66845d
Estéban <e.soubiran25@gmail.com>
2023-08-25 16:19:48 +0200

    fix: optimize blog styles (#72)

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import type { BlogPostCard } from 'types/blog'
 
-const { data } = await useAsyncData('blog', () => queryContent('/blog/').only(['_path', 'cover', 'title', 'description', 'publishedAt', 'authors']).find() as Promise<BlogPostCard[]>)
+const { data } = await useAsyncData('blog', () => queryContent('/blog/').only(['_path', 'cover', 'title', 'description', 'publishedAt', 'authors']).sort({ publishedAt: -1 }).find() as Promise<BlogPostCard[]>)
 </script>
 
 <template>
