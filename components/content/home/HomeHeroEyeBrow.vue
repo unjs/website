b53d1b8a908e2dde3e0c1c30db4a484444cdf30b
Estéban <e.soubiran25@gmail.com>
2023-08-29 19:06:05 +0200

    fix: home eyebrow (#82)

--- components/content/home/HomeHeroEyeBrow.vue
+++ components/content/home/HomeHeroEyeBrow.vue
@@ -7,13 +7,13 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="`${path}?utm_source=unjs.io&utm_medium=home-hero`" flex="~" gap-4>
-    <span p="x-3 y-1" bg="gray-900 opacity-10" border="~ gray-900 opacity-40" rounded="full" text="sm gray-900" font="medium">
+  <NuxtLink :to="`${path}?utm_source=unjs.io&utm_medium=home-hero`" flex="~ items-center" gap-4>
+    <span p="x-3 y-1" bg="gray-900 opacity-10" border="~ gray-900 opacity-40" rounded="full" text="sm gray-900 nowrap" font="medium">
       {{ prefix }}
     </span>
     <span flex="~ row items-center" gap-2>
-      {{ title }}
-      <span i-heroicons-chevron-right-20-solid w-5 h-5 />
+      <span text="left">{{ title }}</span>
+      <span shrink="0" i-heroicons-chevron-right-20-solid w-5 h-5 />
     </span>
   </NuxtLink>
 </template>
