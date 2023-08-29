3a6e5e1e3c0232f5f2ef7decf6e483e9f7f7505c
barbapapazes <e.soubiran25@gmail.com>
2023-08-29 15:41:19 +0200

    feat: create og-image for nitro

--- components/OgImageTemplate/Package.vue
+++ components/OgImageTemplate/Package.vue
@@ -8,19 +8,31 @@ defineProps<{
   owner: string
   repo: string
   description: string
+  logo: string
 }>()
 </script>
 
 <template>
-  <div class="relative w-full pt-[12.5rem] pb-[6.25rem] px-[8.75rem] flex flex-col bg-white">
-    <h1 class="my-0 text-9xl text-gray-900 font-normal flex flex-row gap-2">
-      <span>{{ owner }}</span>
-      <span>/</span>
-      <span class="font-bold">{{ repo }}</span>
-    </h1>
-    <p class="mt-8 text-gray-500 text-[3.5rem] font-medium">
-      {{ description }}
-    </p>
-    <div class="absolute bottom-0 left-0 right-0 h-8 bg-[#ECDC5A]" />
+  <!-- This use Tailwind from Satori, not Uno CSS -->
+  <div class="w-full flex flex-col bg-white">
+    <div class="grow pt-[12.5rem] pb-[8.25rem] px-[6.75rem] flex flex-col justify-between">
+      <div class="flex flex-row justify-between">
+        <div>
+          <h1 class="my-0 text-9xl text-gray-900 font-normal flex flex-row">
+            <span>{{ owner }}</span>
+            <span class="ml-2">/</span>
+            <span class="ml-2 font-bold">{{ repo }}</span>
+          </h1>
+          <p class="mt-20 text-gray-500 text-[3.5rem] font-medium">
+            {{ description }}
+          </p>
+        </div>
+        <img :src="logo" width="300" height="300" class="ml-20 w-[300px] h-[300px]">
+      </div>
+      <div class="flex flex-row justify-end">
+        <img src="/favicon.svg" width="100" height="100" class="rounded-[0.25rem] w-[100px] h-[100px]">
+      </div>
+    </div>
+    <div class="w-full h-8 bg-[#ECDC5A]" />
   </div>
 </template>
--- layouts/package.vue
+++ layouts/package.vue
@@ -5,12 +5,15 @@ const documentation = computed(() => page.value.documentation)
 const owner = computed(() => page.value.github.owner)
 const repo = computed(() => page.value.github.repo)
 const description = computed(() => page.value.description)
+// TODO: export every icon from icones to use them as an icon in every packages
+const logo = computed(() => page.value.logo)
 
 defineOgImage({
   component: 'Package',
   owner: owner.value,
   repo: repo.value,
   description: description.value,
+  logo: logo.value,
 })
 </script>
 
