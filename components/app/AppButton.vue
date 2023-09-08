aae9789f7de5737e7df6e816244a32602a88ef18
Estéban <e.soubiran25@gmail.com>
2023-09-08 13:43:27 +0200

    fix: update package page (#103)

---
+++ components/app/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLink p="x-3 y-2" rounded="0.375rem" bg="primary hover:op-80" flex="~ justify-center items-center" gap-2 text-gray-900 transition="~ ease-in duration-150">
+    <slot />
+  </NuxtLink>
+</template>
