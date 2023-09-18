f29d7e24db5aa79ec5dce1fee97f3a183c029ea8
barbapapazes <e.soubiran25@gmail.com>
2023-09-18 12:52:21 +0200

    refactor: home page

--- components/content/home/HomeTheUnjsUniverse.vue
+++ components/content/home/HomeTheUnjsUniverse.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { PuzzlePart } from 'types/puzzle'
+import type { PuzzlePart } from '~/types/puzzle'
 
 defineProps<{
   cta: { to: string; text: string }
@@ -8,20 +8,16 @@ defineProps<{
 </script>
 
 <template>
-  <section flex="~ col" gap-6>
-    <div px-6 py-8 bg-white rounded-4 border border-light>
-      <div max-w-3xl flex="~ col" gap-4>
-        <p md:text-xl text-gray-700 font-semibold tracking-wide>
-          <ContentSlot :use="$slots.eyebrow" unwrap="p" />
-        </p>
-        <h2 text-2xl md:text-3xl lg:text-4xl text-gray-900 font-extrabold tracking-wide>
-          <ContentSlot :use="$slots.title" unwrap="p" />
-        </h2>
-        <p text-lg md:text-2.5xl md:leading-tight text-gray-600>
-          <ContentSlot :use="$slots.subtitle" unwrap="p" />
-        </p>
-      </div>
-    </div>
+  <HomeSection>
+    <template #eyebrow>
+      <ContentSlot :use="$slots.eyebrow" unwrap="p" />
+    </template>
+    <template #title>
+      <ContentSlot :use="$slots.title" unwrap="p" />
+    </template>
+    <template #subtitle>
+      <ContentSlot :use="$slots.subtitle" unwrap="p" />
+    </template>
     <div relative overflow-hidden>
       <HomeTheUnjsUniverseCarousel :parts="carousel" />
     </div>
@@ -33,5 +29,5 @@ defineProps<{
         <span i-heroicons-chevron-right-20-solid w-5 h-5 />
       </NuxtLink>
     </div>
-  </section>
+  </HomeSection>
 </template>
