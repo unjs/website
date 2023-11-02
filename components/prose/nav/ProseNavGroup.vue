5328e7da77a58ba6cc7c8a2f91657e197dc0480c
barbapapazes <e.soubiran25@gmail.com>
2023-11-02 23:36:22 +0100

    chore: lint

--- components/prose/nav/ProseNavGroup.vue
+++ components/prose/nav/ProseNavGroup.vue
@@ -15,13 +15,14 @@ const id = useProseNavGroupId()
       <div class="hidden md:block">
         <slot name="title" />
       </div>
-      <UButton v-if="!noDisclosure" color="gray" variant="ghost" class="flex items-center gap-2 md:hidden" @click="open = !open" @keyup.spacebar="open = !open" type="button" :aria-expanded="open" :aria-controls="id" :ui="{ padding: { sm: 'px-0 py-0' }, color: { gray: { ghost: 'hover:bg-transparent dark:hover:bg-transparent' } } }" >
+      <!-- eslint-disable-next-line vue/valid-v-on -->
+      <UButton v-if="!noDisclosure" color="gray" variant="ghost" class="flex items-center gap-2 md:hidden" type="button" :aria-expanded="open" :aria-controls="id" :ui="{ padding: { sm: 'px-0 py-0' }, color: { gray: { ghost: 'hover:bg-transparent dark:hover:bg-transparent' } } }" @click="open = !open" @keyup.space-bar="open = !open">
         <span class="w-4 h-4" :class="icon" />
         <slot name="title" />
         <span class="i-heroicons-chevron-right-20-solid w-4 h-4 transition ease-in" :class="{ 'transform rotate-90': open }" />
       </UButton>
     </div>
-    <div :id="id" class="mt-2 text-zinc-500 md:block" :class="{'hidden': !open && !noDisclosure}">
+    <div :id="id" class="mt-2 text-zinc-500 md:block" :class="{ hidden: !open && !noDisclosure }">
       <slot name="links" />
     </div>
   </div>
