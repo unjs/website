fc4dfec372fbdd5c0c514ae13d87041819a16a4f
Estéban <e.soubiran25@gmail.com>
2023-11-03 14:45:09 +0100

    fix: remove dot in blog post (#170)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
     prerender: {
       failOnError: false,
       crawlLinks: true,
-      routes: ['/', '/api/search.txt', '/rss.xml', '/rss.xml', '/blog/rss.xml', '/learn/rss.xml', '/explore/rss.xml', '/build/rss.xml'],
+      routes: ['/', '/blog', '/packages', '/api/search.txt', '/rss.xml', '/rss.xml', '/blog/rss.xml', '/learn/rss.xml', '/explore/rss.xml', '/build/rss.xml'],
     },
     routeRules: {
       '/api/github/**': {
@@ -50,6 +50,12 @@ export default defineNuxtConfig({
       '/api/npm/**': {
         cache: {
           maxAge: 60 * 60 * 24 * 7, // 7 days
+        },
+      },
+      '/blog/2023-08-25-nitro-2.6': {
+        redirect: {
+          statusCode: 301,
+          to: '/blog/2023-08-25-nitro-2-6',
         },
       },
     },
