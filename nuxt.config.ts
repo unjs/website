d302cfc2f988b9abd3bff8dad9be6e6cbbc124c9
Estéban <e.soubiran25@gmail.com>
2023-11-07 14:33:28 +0000

    fix: disable og image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -86,6 +86,9 @@ export default defineNuxtConfig({
   linkChecker: {
     enabled: false,
   },
+  ogImage: {
+    enabled: false,
+  },
   routeRules: {
     ...packagesRedirects as NitroConfig['routeRules'],
   },
