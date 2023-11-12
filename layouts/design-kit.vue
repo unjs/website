c1e1d119f3b7ed3c59219edb8c78948258576eb7
barbapapazes <e.soubiran25@gmail.com>
2023-11-12 21:49:25 +0100

    feat: design kit

---
+++ layouts/design-kit.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const { page } = useContent()
+</script>
+
+<template>
+  <Main>
+    <AppHero :title="page.title" :description="page.description" />
+
+    <!--
+      le nom et la manière dont on l'écrit
+     -->
+     <!--
+      le logo (fond blanc et noir)
+      -->
+      <!--
+        Les 3 couleurs principales (jaune, noir et blanc)
+       -->
+      <!--
+        la typographie
+       -->
+  </Main>
+</template>
