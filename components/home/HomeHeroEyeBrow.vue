cfbe8a4fc535788b606a0eac826bcf1538aa4f2c
barbapapazes <e.soubiran25@gmail.com>
2023-12-01 19:53:28 +0100

    fix: home eye brow

--- components/home/HomeHeroEyeBrow.vue
+++ components/home/HomeHeroEyeBrow.vue
@@ -7,11 +7,11 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="`${path}?utm_source=unjs.io&utm_medium=home-hero`" class="flex items-center gap-4">
-    <span class="px-3 py-1 dark:bg-gray-700/40 ring-1 ring-gray-700 dark:ring-gray-700 rounded-full text-sm text-gray-950 dark:text-gray-50 nowrap font-medium">
+  <NuxtLink :to="`${path}?utm_source=unjs.io&utm_medium=home-hero`" class="group flex items-center gap-4">
+    <span class="px-3 py-1 bg-gray-300/20 group-hover:bg-gray-300/40 dark:bg-gray-700/40 dark:group-hover:bg-gray-700/50 ring-1 ring-gray-300 dark:ring-gray-700 rounded-full text-sm text-gray-950 dark:text-gray-50 nowrap font-medium transition ease-in">
       {{ prefix }}
     </span>
-    <span class="flex items-center gap-2 text-gray-500 dark:text-gray-400">
+    <span class="flex items-center gap-2 text-gray-500 group-hover:text-gray-600 dark:text-gray-400 dark:group-hover:text-gray-300 transition ease-in">
       <span class="text-left">{{ title }}</span>
       <span class="shrink-0 w-5 h-5 i-heroicons-chevron-right-20-solid" />
     </span>
