8980b42b9f4792d4f9608f71d48996ff3b27da44
Akshara Hegde <aksharadt@skiff.com>
2024-01-09 20:54:35 +0530

    fix: #220 og-image - spreading gradient background

--- components/OgImage/BlogOgImage.vue
+++ components/OgImage/BlogOgImage.vue
@@ -21,28 +21,25 @@ const props = withDefaults(
 <template>
   <div
     class="w-full h-full flex justify-between relative bg-gray-900 text-white"
-    :style="{
-      background: Boolean(coverImage) ? `url(${coverImage})` : '',
-    }"
   >
-  <svg class="absolute top-0 right-0 h-[85%]" viewBox="0 0 1289 1036" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg class="absolute top-0 right-0 w-[80%] h-[98%]" viewBox="0 0 1289 1036" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g filter="url(#filter0_f_653_7294)">
-    <ellipse cx="865.5" cy="328" rx="625.5" ry="468" fill="url(#paint0_linear_653_7294)" fill-opacity="0.5"/>
+      <ellipse cx="865.5" cy="328" rx="625.5" ry="468" fill="url(#paint0_linear_653_7294)" fill-opacity="0.9"/>
     </g>
     <defs>
-    <filter id="filter0_f_653_7294" x="0" y="-380" width="1731" height="1416" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-    <feGaussianBlur stdDeviation="120" result="effect1_foregroundBlur_653_7294"/>
-    </filter>
-    <linearGradient id="paint0_linear_653_7294" x1="1192.56" y1="276" x2="589.497" y2="941.118" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#ECDC5A"/>
-    <stop offset="1" stop-color="#FBE84C" stop-opacity="0"/>
-    </linearGradient>
+      <filter id="filter0_f_653_7294" x="0" y="-380" width="1731" height="1416" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feGaussianBlur stdDeviation="120" result="effect1_foregroundBlur_653_7294"/>
+      </filter>
+      <linearGradient id="paint0_linear_653_7294" x1="1192.56" y1="276" x2="589.497" y2="941.118" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#ECDC5A"/>
+        <stop offset="1" stop-color="#FBE84C" stop-opacity="0"/>
+      </linearGradient>
     </defs>
   </svg>
 
-    <div class="p-[60px]" style="background: linear-gradient(to left, rgb(55, 65, 81, 0.5), rgb(17, 24, 39, 0.8), rgb(0, 0, 0))">
+    <div class="p-[60px]" style="background: linear-gradient(to right, rgb(17, 24, 39, 1), rgb(17, 24, 39, 0.6), rgb(17, 24, 39, 0.1))">
       <div class="h-full w-full justify-between relative">
         <div class="flex flex-row justify-between items-start">
           <div class="flex flex-col w-full">
@@ -55,7 +52,7 @@ const props = withDefaults(
             </p>
           </div>
         </div>
-        <div class="flex justify-start items-start text-left">
+        <div class="flex justify-start items-start text-left pb-2">
           <img src="/favicon.svg" height="100" />
         </div>
       </div>
