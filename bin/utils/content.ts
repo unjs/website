8188798dda31a95c74011ed7e73f7c9ba4787e77
Estéban <e.soubiran25@gmail.com>
2024-01-14 12:10:51 +0100

    content: add examples to packages (#238)

--- bin/utils/content.ts
+++ bin/utils/content.ts
@@ -1,7 +1,9 @@
 import { readFileSync, readdirSync, rmSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { cwd } from 'node:process'
-import type { GitHubRepo } from '../types'
+import yaml from 'js-yaml'
+import { ofetch } from 'ofetch'
+import type { ContentPackage, GitHubFile } from '../types'
 
 export function getContentPath() {
   const currentPath = cwd()
@@ -21,13 +23,21 @@ export function getBlogPath() {
   return join(contentPath, '5.blog')
 }
 
+export function loadPackageContent(name: string) {
+  return yaml.load(readFileSync(join(getPackagesPath(), `${name}.yml`), 'utf-8')) as ContentPackage
+}
+
+export function writePackageContent(package_: ContentPackage) {
+  writeFileSync(join(getPackagesPath(), `${package_.title}.yml`), yaml.dump(package_))
+}
+
 export function getPackages() {
   const packagesPath = getPackagesPath()
 
   return readdirSync(packagesPath).filter(p => p.endsWith('.yml') && !p.startsWith('.')).map(p => p.replace('.yml', ''))
 }
 
-export function addPackage(repo: GitHubRepo) {
+export function addPackage(repo: { name: string, description: string, examples: string | null }) {
   const packagesPath = getPackagesPath()
 
   const template = readFileSync(join(packagesPath, '.template.yml'), 'utf-8')
@@ -37,6 +47,7 @@ export function addPackage(repo: GitHubRepo) {
     .replace('package_description', repo.description)
     .replace('repo_name', repo.name)
     .replace('npm_name', repo.name)
+    .replace('examples_link', repo.examples || 'null')
     .replace('docs_link', `https://github.com/unjs/${repo.name}`))
 }
 
@@ -44,4 +55,12 @@ export function removePackage(name: string) {
   const packagesPath = getPackagesPath()
 
   rmSync(join(packagesPath, `${name}.yml`))
+}
+
+export async function getExamplesLink(name: string) {
+  const files = await ofetch<{ files: GitHubFile[] }>(`https://ungh.cc/repos/unjs/${name}/files/main`)
+
+  const hasExamples = files.files.some(f => f.path.startsWith('examples/'))
+
+  return hasExamples ? `https://github.com/unjs/${name}/blob/main/examples` : null
 }
