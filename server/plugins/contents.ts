85076fc80a6899998bff1038b4b9987b422df98b
barbapapazes <e.soubiran25@gmail.com>
2024-01-31 00:24:39 +0100

    fix: add missing server plugin

---
+++ server/plugins/contents.ts
@@ -0,0 +1,16 @@
+export default defineNitroPlugin((nitroApp) => {
+  nitroApp.hooks.hook('content:file:afterParse', (file) => {
+    if (file._id.endsWith('.md')) {
+      // Handle GitHub flavoured markdown blockquotes
+      // https://github.com/orgs/community/discussions/16925
+      for (const node of (file.body?.children || [])) {
+        if (node.tag === 'blockquote' && // blockquotes > p x 2 > span > text
+          ['!NOTE', '!TIP', '!IMPORTANT', '!WARNING', '!CAUTION'].includes(node.children?.[0]?.children?.[0]?.children?.[0]?.value)) {
+            node.type = 'element'
+            node.tag = node.children?.[0]?.children?.[0]?.children?.[0]?.value.slice(1).toLowerCase()
+            node.children[0].children.shift()
+          }
+      }
+    }
+  })
+})
