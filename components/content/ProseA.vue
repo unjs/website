8960bf644932433e89ec11e12d604b9dff9394be
barbapapazes <esteban.soubiran@insa-cvl.fr>
2024-01-31 14:58:46 +0100

    fix: prose a

---
+++ components/content/ProseA.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+  target?: string
+  rel?: string
+}>()
+
+const rel = computed(() => {
+  if (props.href.includes('.unjs.io'))
+    return 'noopener'
+
+  return 'noopener noreferrer'
+})
+
+const to = computed(() => {
+  if (props.href.includes('.unjs.io'))
+    return `${props.href}?utm_source=unjs.io&utm_medium=referral&utm_campaign=unjs.io`
+
+  return props.href
+})
+</script>
+
+<template>
+  <NuxtLink
+    :to="to"
+    :target="target"
+    :rel="rel"
+  >
+    <slot />
+  </NuxtLink>
+</template>
