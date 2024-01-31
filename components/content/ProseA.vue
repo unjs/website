66836967aa864d656f2f2a5e00ad224fda5a6179
Estéban <e.soubiran25@gmail.com>
2024-01-31 18:23:12 +0100

    fix: content prose a (#275)

---
+++ components/content/ProseA.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+  target?: string
+  rel?: string
+}>()
+
+const rel = computed(() => {
+  if (props.href.includes('.unjs.io'))
+    return 'noopener'
+
+  return 'noopener noreferrer'
+})
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+    :rel="rel"
+  >
+    <slot />
+  </NuxtLink>
+</template>
