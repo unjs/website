ee1bfd860e1be663850604d3dc0db395e789ba73
barbapapazes <e.soubiran25@gmail.com>
2024-02-19 07:14:14 +0000

    feat: add omark package

--- config/packages-redirects.ts
+++ config/packages-redirects.ts
@@ -224,6 +224,13 @@ export default {
     },
   },
 
+  '/omark': {
+    redirect: {
+      to: '/packages/omark',
+      statusCode: 302,
+    },
+  },
+
   '/pathe': {
     redirect: {
       to: '/packages/pathe',
