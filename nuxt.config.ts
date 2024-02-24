70c43c98bdaec7e2405654fc323877153ea57d29
barbapapazes <e.soubiran25@gmail.com>
2024-02-24 20:25:21 +0100

    fix: update api (readme walker and cache)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,21 +51,6 @@ export default defineNuxtConfig({
       ],
     },
     routeRules: {
-      '/api/content/**': {
-        cache: {
-          maxAge: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-      '/api/github/**': {
-        cache: {
-          maxAge: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-      '/api/npm/**': {
-        cache: {
-          maxAge: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
       '/blog/2023-08-25-nitro-2.6': {
         redirect: {
           statusCode: 301,
--- server/api/github/stars.ts
+++ server/api/github/stars.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async () => {
-  const { repos } = await $fetch<{ repos: { stars: number }[] }>('https://ungh.cc/orgs/unjs/repos')
+  const repos = await cachedGitHubRepos()
 
   const stars = repos.reduce((acc, repo) => acc + repo.stars, 0)
 
