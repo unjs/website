1914aace2dc449418f765bcfa5fc3d69324b9ef7
Estéban <e.soubiran25@gmail.com>
2024-02-28 23:29:57 +0100

    feat: use nuxt/fonts (#315)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,8 +29,7 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@nuxtjs/plausible',
     'nuxt-payload-analyzer',
-    '@nuxtjs/fontaine',
-    '@nuxtjs/google-fonts',
+    '@nuxt/fonts',
   ],
   css: [
     '~/assets/app.css',
@@ -75,16 +74,6 @@ export default defineNuxtConfig({
   },
   colorMode: {
     preference: 'dark',
-  },
-  fontMetrics: {
-    fonts: ['Nunito'],
-  },
-  googleFonts: {
-    display: 'swap',
-    download: true,
-    families: {
-      Nunito: [300, 400, 500, 600, 700, 800],
-    },
   },
   ogImage: {
     fonts: [
